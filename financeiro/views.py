3a760ddbdfa802aff9b9ad9af30a3e46209d6e35
Luis PAulo T. Fernandes <luispaulo1011@gmail.com>
2016-08-19 13:59:17 -0300

    Adicionado a view de detalhes e a view base

--- financeiro/views.py
+++ financeiro/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(data__lte=timezone.now()).order_by('data')
+    posts = Post.objects.filter(data__lte=timezone.now()).order_by('-data')
     return render(request, 'financeiro/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'financeiro/post_detail.html', {'post': post})
