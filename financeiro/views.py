7a8214da16e946d05e9bb04d197f6b50afc6a58b
Luis PAulo T. Fernandes <luispaulo1011@gmail.com>
2016-08-18 23:44:25 -0300

    Criado novas views para mostrar e editar os valores inseridos

--- financeiro/views.py
+++ financeiro/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'financeiro/post_list.html', {})
+    posts = Post.objects.filter(data__lte=timezone.now()).order_by('data')
+    return render(request, 'financeiro/post_list.html', {'posts': posts})
